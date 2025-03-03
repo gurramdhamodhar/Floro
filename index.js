b6eea725eb4ff09aedcf5d8a0697282b40e72ed3
Dhamodhar G <g.dhamodhar5@gmail.com>
2025-03-03 18:35:21 +0530

    updated js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuOpenbtn = document.querySelector(".menuOpenbtn");
 const openMobilemenu = document.querySelector(".openMobilemenu");
 const closeDivbtn = document.querySelector(".closeDivbtn");
+const menuLinks = document.querySelectorAll(".openMobilemenu a");
 
 menuOpenbtn.addEventListener("click", () => {
   openMobilemenu.classList.remove("translate-x-full");
@@ -8,4 +9,10 @@ menuOpenbtn.addEventListener("click", () => {
 
 closeDivbtn.addEventListener("click", () => {
   openMobilemenu.classList.add("translate-x-full");
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    openMobilemenu.classList.add("translate-x-full");
+  });
 });
